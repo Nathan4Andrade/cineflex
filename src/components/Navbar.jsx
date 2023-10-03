8d1c030d87547e910f78e4cf5ab47eaae7590b83
julio <mikapiccol@gmail.com>
2023-10-03 12:08:54 -0300

    ajustes css e efeitos no home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Return = styled.button`
 `;
 const NavContainer = styled.div`
   width: 100%;
-  height: 70px;
+  height: 100px;
   display: flex;
   align-items: center;
   justify-content: center;
